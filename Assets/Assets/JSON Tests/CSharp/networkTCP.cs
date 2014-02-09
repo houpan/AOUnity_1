fada7d5eba10bfc499bdcfed8e5b3bcea90e070a
厚片 <mhci@HoupanMacbookAir.local>
2014-02-08 21:30:48 -0500

    backup push

---
+++ Assets/Assets/JSON Tests/CSharp/networkTCP.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets; 
+using System.Text;
+
+public class scrff : MonoBehaviour {
+
+	NetworkStream clientStream;
+	// Use this for initialization
+	void Start () {
+	
+		TcpClient tcpClient = new TcpClient(); 
+		tcpClient.Connect("127.0.0.1", 10000);
+
+		clientStream = tcpClient.GetStream(); 
+		byte[] message = new byte[4096];
+		int bytesRead;
+		bytesRead = 0;
+		bytesRead = clientStream.Read(message, 0, 4096);
+
+		UTF8Encoding encoder = new UTF8Encoding();
+		MonoBehaviour.print(encoder.GetString(message, 0, bytesRead)); 
+
+
+
+	}
+	int where = 0;
+	// Update is called once per frame
+	void Update () {
+		where ++;
+		byte[] outStream = Encoding.UTF8.GetBytes("___"+ where );
+		clientStream.Write(outStream, 0, outStream.Length);
+		clientStream.Flush();
+		Debug.Log("sent!");
+	
+	}
+}
--- Assets/Assets/networkTCP.cs
+++ Assets/Assets/networkTCP.cs
@@ -8,33 +8,17 @@ using System.Threading;
 public class TCPCommunicator
 {
 
-    // This method will be called when the thread is started.
-    public void DoWork()
-    {
-    }
-    public void RequestStop()
-    {
-    }
-    // Volatile is used as hint to the compiler that this data
-    // member will be accessed by multiple threads.
-    private volatile bool _shouldStop;
-}
-
-public class networkTCP : MonoBehaviour {
-
 	TcpClient tcpClient;
 	NetworkStream clientStream;
 	UTF8Encoding encoder;
 	bool waiting = false;
-    Thread listenerThread;
+	bool isAlive = false;
 
 
-
-	// Use this for initialization
-	void Start () {
-	
+    public TCPCommunicator(){
 		tcpClient = new TcpClient(); 
 		tcpClient.Connect("54.250.127.255", 10000);
+
 
 		clientStream = tcpClient.GetStream(); 
 		byte[] message = new byte[4096];
@@ -57,19 +41,16 @@ public class networkTCP : MonoBehaviour {
         I.Add("version", "1.0");
         var stringfied = I.ToString();
 
-
         sendMessageToServerUpon(stringfied);
 /*		byte[] outStream = Encoding.UTF8.GetBytes("__"+stringfied);
 		clientStream.Write(outStream, 0, outStream.Length);
 		clientStream.Flush();*/
 
-		Debug.Log("sent!");
-
-        listenerThread = new Thread(acceptance);
-        listenerThread.Start();
+		Debug.Log("sendFirstMessageToServer!");
 
 
-	}
+    }
+
 	void sendMessageToServerUpon(string stringInput){
 		int bytesRead;
 		byte[] outStream = Encoding.UTF8.GetBytes("__"+stringInput);
@@ -77,63 +58,71 @@ public class networkTCP : MonoBehaviour {
 		clientStream.Flush();		
 	}
 
-	void acceptance(){
-    
+	public void listeningToServerMessage(){
+		byte[] message = new byte[4096];
+		int bytesRead;
+		bytesRead = 0;
+		while(true){
+			sendMessageToServerUpon("Unity is waiting for msg!");
+			Debug.Log("unity is waiting!");
+			bytesRead = clientStream.Read(message, 0, 4096);
+			clientStream.Flush();
 
-			byte[] message = new byte[4096];
-			int bytesRead;
-			bytesRead = 0;
-			while(true){
-
-				sendMessageToServerUpon("Unity is waiting for msg!");
-				Debug.Log("unity is waiting!");
-				bytesRead = clientStream.Read(message, 0, 4096);
-				clientStream.Flush();
-				MonoBehaviour.print(encoder.GetString(message, 0, bytesRead));	
-
-				sendMessageToServerUpon("Unity got your msg!");
-				Debug.Log("unity get!");
-			}
-/*
-     do{
-          numberOfBytesRead = myNetworkStream.Read(myReadBuffer, 0, myReadBuffer.Length);  
-          myCompleteMessage = 
-              String.Concat(myCompleteMessage, Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));  
-     }
-     while(myNetworkStream.DataAvailable);
-     */
+			sendMessageToServerUpon("Unity got your msg!");
+			Debug.Log("unity get!::");
+			MonoBehaviour.print(encoder.GetString(message, 0, bytesRead));	
+		}
 
 	}
+
+    // This method will be called when the thread is started.
+    public void DoWork()
+    {
+    }
+    public void RequestStop()
+    {
+    }
+    // Volatile is used as hint to the compiler that this data
+    // member will be accessed by multiple threads.
+    private volatile bool _shouldStop;
+}
+
+public class networkTCP : MonoBehaviour {
+
+
+
+	TCPCommunicator TCPCommunicatorObject;
+    Thread TCPCommunicatorlistenerThread;
+
+	// Use this for initialization
+	void Start () {
+		TCPCommunicatorObject = new TCPCommunicator();
+
+		TCPCommunicatorlistenerThread = new Thread(TCPCommunicatorObject.listeningToServerMessage);
+        TCPCommunicatorlistenerThread.Start();
+        while(!TCPCommunicatorlistenerThread.IsAlive);//讓程式把這個thread開起來候再作事
+
+
+	}
+
+
+
 
 	// Update is called once per frame
 	void Update () {
 
-
-
-		 
 /*
-        var I = new JSONClass();
-        I["version"].AsInt = 5;
-        I["author"]["name"] = "Bunny83";
-        I["author"]["phone"] = "0123456789";
-        I["data"][-1] = "First item\twith tab";
-        I["data"][-1] = "Second item";
-        I["data"][-1]["value"] = "class item";
-        I["data"].Add("Forth item");
-        I["data"][1] = I["data"][1] + " 'addition to the second item'";
-        I.Add("version", "1.0");
-        var stringfied = I.ToString();
+        // Request that the worker thread stop itself:
+        TCPCommunicatorObject.RequestStop();
 
-		byte[] outStream = Encoding.UTF8.GetBytes("__"+stringfied+"_"+ where );
-		clientStream.Write(outStream, 0, outStream.Length);
-		clientStream.Flush();
-		Debug.Log("sent!");
-*/
-	
+        // Use the Join method to block the current thread 
+        // until the object's thread terminates.
+        TCPCommunicatorObject.Join();
+		Debug.Log("main thread: Worker thread has terminated.");
+		*/
 	}
 
 	void OnDestroy () {
-		print("Script was destroyed");
 	}
 
 }
